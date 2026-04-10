4ff9f11cf635d6d292da5eba1ec04913f1522213
paola <purplehoppe04@gmail.com>
2026-04-10 10:23:06 -0500

    subiendo ejer1 en c#

---
+++ SEM3-C/ejerc1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM3_C
+{
+    internal class ejerc1
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+            Console.Write("Ingrese nombre:");
+            Console.Write("Ingrese carrera:");
+
+        }
+    }
+}
---
+++ SEM3-C/ejerc2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM3_C
+{
+    internal class ejerc2
+    {
+    }
+}
