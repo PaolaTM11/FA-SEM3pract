035480a4956830890c5302cb6487a6cdb3b99672
paola <purplehoppe04@gmail.com>
2026-04-17 10:37:44 -0500

    avance de ejercicio en py hasta el 3

---
+++ SEM3-P/EJER3.py
@@ -0,0 +1,8 @@
+num1= int(input("Ingrese número 1: "));
+num2= int(input("Ingrese número 2: "));
+    
+
+print(f"\nSuma:{num1 + num2}");
+print.WriteLine(f"Resta:{num1 - num2}");
+print.WriteLine(f"Multiplicacion:{num1 * num2}");
+print(f"Division:{num1 / num2}");
