2216b2d93af4164903e732f8706c0c01c1a2dd9d
paola <purplehoppe04@gmail.com>
2026-04-24 10:38:04 -0500

    Subiendo la semana 4

---
+++ Semana4/ejerci2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana4
+{
+    internal class ejerci2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el año: ");
+            int an = int.Parse(Console.ReadLine());
+
+            if ((an % 4 ==0 & an % 100 !=0) || an%400 ==0)
+           {
+             Console.WriteLine("\nEl año es bisiesto");
+           } 
+            else Console.WriteLine("\nEl año no es bisiesto");
+           
+           if (an % 2 == 0) Console.WriteLine("\nEl año es par");
+            else Console.WriteLine("\nEl año es impar");
+        }
+    }
+}
---
+++ Semana4/ejerci3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana4
+{
+    internal class ejerci3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("---Sistema de conversión monetario--- \n ");
+
+            Console.Write("\nIngrese el monto en soles: ");
+            double mo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n1. Convertir a dolares");
+            Console.WriteLine("2. Convertir a Euros\n");
+
+            Console.WriteLine("Ingrese una opción: ");
+            int opc = int.Parse(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1:
+                    double dolares = mo / 3.75;
+                    Console.WriteLine("\nDolares: " + dolares);
+                    break;
+                case 2:
+                    double euros = mo / 4.95;
+                    Console.WriteLine("\nEuros:" + euros);
+                    break;
+                default:
+                    Console.WriteLine("Opción incorrecta!");
+                    break;
+
+
+            }
+
+        }
+    }
+}
